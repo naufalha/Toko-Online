0538affbb0bdd547cd882c608f40710dc64629d1
l200210135 <l200210135@student.ums.ac.id>
2023-06-21 20:51:04 +0700

    home

--- index.php
+++ index.php
@@ -49,6 +49,35 @@
   <span class="dot"></span> 
 </div>
 
+
+
+<div>
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "toko");
+
+// Periksa koneksi
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal: " . mysqli_connect_error();
+    exit;
+}
+
+$sql = "SELECT * FROM barang";
+$result = mysqli_query($koneksi, $sql);
+if ($result) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<img  src=".$row["foto"]."><br>";
+        echo "id: " . $row["login_id"]. " - Name: " . $row["nama"]. " " . $row["harga"]. "<br>";
+        echo "<a href='barang.php?idbarang=".$row['idbarang']."'>Beli</a> <br>";
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($koneksi);
+?>
+</div>
+
+
 <script>
 let slideIndex = 0;
 showSlides();
@@ -69,28 +98,7 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 
-  <?php
-$koneksi = mysqli_connect("localhost", "root", "", "toko");
 
-// Periksa koneksi
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal: " . mysqli_connect_error();
-    exit;
-}
-
-$sql = "SELECT * FROM barang";
-$result = mysqli_query($koneksi, $sql);
-if ($result) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "img src=".$row["foto"]."<br>";
-        echo "id: " . $row["login_id"]. " - Name: " . $row["nama"]. " " . $row["harga"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-?>
   
 }
 </script>
