6ba2e01e35f429b674c58ff380a9c4b4018d346a
l200210135 <l200210135@student.ums.ac.id>
2023-06-21 14:06:23 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Toko-Online</title>
+</head>
+<body>
+<h1>Selamat Datang di Toko Online</h1>
+<p>
+<a href="login.php">Login</a> | <a href="register.php">Register</a>
+</p>
+</body>
+
+
+
+</html>
