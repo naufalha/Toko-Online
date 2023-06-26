feda98fe30bd6e514737cafd1a9c6522556f894e
MaulanaFred21 <113358690+MaulanaFred21@users.noreply.github.com>
2023-06-26 19:43:07 +0700

    modifikasi tampilan input barang dengan css

--- jual.php
+++ jual.php
@@ -2,27 +2,118 @@
 <html>
 <head>
     <title>Form Upload Barang</title>
-
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: white;
+            margin: 0;
+            padding: 20px;
+        }
+        
+        h2 {
+            color: #333333;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: wheat;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            display: flex;
+            flex-direction: column;
+        }
+        
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        
+        input[type="text"],
+        textarea {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #cccccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        
+        textarea {
+            resize: vertical;
+            height: 100px;
+        }
+        
+        input[type="file"] {
+            display: block;
+            margin-top: 5px;
+        }
+        
+        .button-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 20px;
+        }
+        
+        .button-container input[type="submit"] {
+            background-color: #1E90FF;
+            color: #ffffff;
+            padding: 10px 16px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+        
+        .button-container input[type="submit"]:hover {
+            background-color: #483D8B;
+        }
     
+    </style>
 </head>
 <body>
-    <h2>Form Upload Barang</h2>
+    <header>
+        <nav class="container-fluid">
+            <div class="logo">
+                <img src="logo.png" alt="Tokopedia Logo">
+            </div>
+        </nav>
+    </header>
     <form action="insert_jual.php" method="POST" enctype="multipart/form-data">
-        <label for="nama">Nama Barang:</label>
-        <input type="text" name="nama" id="nama" required><br><br>
-
-        <label for="harga">Harga Barang:</label>
-        <input type="text" name="harga" id="harga" required><br><br>
-
-        <label for="deskripsi">Deskripsi Barang:</label><br>
-        <textarea name="deskripsi" id="deskripsi" rows="5" cols="30" required></textarea><br><br>
-
-        <label for="gambar">Gambar Barang:</label>
-        <input type="file" name="gambar" id="gambar" required><br><br>
-
-        
-
-        <input type="submit" value="Upload">
+        <div class="container">
+            <h2>Form Upload Barang</h2>
+            <div class="row">
+                <div class="col">
+                    <label for="nama">Nama Barang:</label>
+                    <input type="text" name="nama" id="nama" required>
+                </div>
+                <div class="col">
+                    <label for="harga">Harga Barang:</label>
+                    <input type="text" name="harga" id="harga" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="deskripsi">Deskripsi Barang:</label><br>
+                    <textarea name="deskripsi" id="deskripsi" rows="5" cols="30" required></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="gambar">Gambar Barang:</label>
+                    <input type="file" name="gambar" id="gambar" required>
+                </div>
+            </div>
+            <div class="button-container">
+                <input type="submit" value="Upload">
+            </div>
+        </div>
     </form>
 </body>
 </html>
